30fd95f99455c7e57d63aed51f0fc470d7677933
stewartbryson <stewartbryson@gmail.com>
2023-01-05 13:24:30 -0500

    Corrected tests and examples.

--- examples/groovy-jar/src/main/groovy/Sample.groovy
+++ examples/groovy-jar/src/main/groovy/Sample.groovy
@@ -1,7 +1,7 @@
 class Sample {
   String addNum(Integer num1, Integer num2) {
     try {
-      "The sum is: ${(num1 + num2)}"
+      "Sum is: ${(num1 + num2)}"
     } catch (Exception e) {
       null
     }
